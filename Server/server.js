a22d8f1e93476615a8c0a951d97d1e89e83fa93c
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-01 20:15:12 +0530

    shifted server to server folder with express server

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const dotenv =require("dotenv");
+const connectDB =require("./config/database.js");
+
+
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main thing
+
+  app.get("/", (req, res) =>
+    res.send("Server is Running")
+  );
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server  is started on port ${PORT}..`
+  )
+);
