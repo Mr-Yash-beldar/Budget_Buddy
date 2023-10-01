8d69ef272e0a68d3c9119a9805078f445ee8c797
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-01 20:23:59 +0530

    mongodb database connection code with mongoose

---
+++ Server/config/database.js
@@ -0,0 +1,18 @@
+//Connection file to mongo db
+const mongoose= require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      //useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit();
+  }
+};
+
+module.exports=connectDB;
