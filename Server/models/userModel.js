949abf894ecc04109bcc7734bd98779c20ae32ae
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-02 16:22:37 +0530

    password checking with encrypted password with bcrypt package

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -48,15 +48,3 @@ userSchema.methods.matchPassword=async function (enteredPassword){
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// // // will encrypt password everytime its saved
-// // userSchema.pre("save", async function (next) {
-// //   if (!this.isModified("password")) {
-// //     next();
-// //   }
-// //   const salt = await bcrypt.genSalt(10);
-// //   this.password = await bcrypt.hash(this.password, salt);
-// // });
