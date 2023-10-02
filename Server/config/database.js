db4a9adbacbd40034268ae7822b84e0dbb8d8fc7
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-02 14:44:45 +0530

    database connection guys

--- Server/config/database.js
+++ Server/config/database.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       //useCreateIndex: true,
     });
-    console.log(`MongoDB Connected: ${conn}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit();
