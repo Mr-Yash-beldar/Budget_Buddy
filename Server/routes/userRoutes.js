69e87e37e3439a7d9fe8f7a27ae7ee8716032f9a
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-02 16:12:28 +0530

    user login route added

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser,authUser } = require("../controllers/userControllers");
 
 const router=express.Router();
 
 router.route('/').post(registerUser);
+router.route('/login').post(authUser);
 
 module.exports=router;
