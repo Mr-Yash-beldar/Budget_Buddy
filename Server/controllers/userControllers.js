769379f610502c3ccfd5eb583065d5ccf30a6521
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-02 16:09:43 +0530

    added token generating function for authrization of user

--- Server/controllers/userControllers.js
+++ Server/controllers/userControllers.js
@@ -1,5 +1,6 @@
-const asyncHandler=require("express-async-handler");
-const User=require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken=require("../Token/generateToken");
 
 const registerUser = async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -25,14 +26,34 @@ const registerUser = async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       pic: user.pic,
-    //   token: generateToken(user._id),
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
     throw new Error("User not found");
   }
 };
+
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
   
+    const user = await User.findOne({ email });
+  
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        pic: user.pic,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Email or Password");
+    }
+  });
+
 module.exports = {
-  registerUser
+  registerUser,authUser
 };
