7d69baf8b8d76f93ac38b910c57646d09037a2c6
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-10-02 14:45:26 +0530

    user database models

---
+++ Server/models/userModel.js
@@ -0,0 +1,59 @@
+const mongoose=require("mongoose");
+const bcrypt =require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports= User;
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// // // will encrypt password everytime its saved
+// // userSchema.pre("save", async function (next) {
+// //   if (!this.isModified("password")) {
+// //     next();
+// //   }
+// //   const salt = await bcrypt.genSalt(10);
+// //   this.password = await bcrypt.hash(this.password, salt);
+// // });
+
