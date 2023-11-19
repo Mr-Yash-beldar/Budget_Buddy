88e2be7d0519aace7ceedcf5a945edfbd88271b9
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-11-20 03:03:25 +0530

    teammebers data file

---
+++ budget-buddy/src/teamMembers.js
@@ -0,0 +1,52 @@
+const teamMembers = [
+  {
+    name: "Yashodip Beldar",
+    job: "Marketing Head",
+    socialMedia: {
+      facebook: "#", // Replace with actual Facebook link
+      twitter: "#", // Replace with actual Twitter link
+      linkedin: "#", // Replace with actual LinkedIn link
+      instagram: "#", // Replace with actual Instagram link
+      youtube: "#", // Replace with actual YouTube link
+    },
+    imageSrc: "assets/images/team-1.jpg", // Replace with actual image source
+  },
+  {
+    name: "Rushikesh Patil",
+    job: "Backend",
+    socialMedia: {
+      geekforgeeks: "https://auth.geeksforgeeks.org/user/rushikeshpaicvf",
+      linkedin: "https://www.linkedin.com/in/rushikesh-patil-36339328a/",
+      leetcode: "https://leetcode.com/rushi173/",
+      github: "https://github.com/rushi173",
+    },
+    imageSrc:
+      "https://i.postimg.cc/2SssHF5y/rushikesh.jpg",
+  },
+  {
+    name: "Damini Kalal",
+    job: "Frontend Developer",
+    socialMedia: {
+      geekforgeeks: "https://auth.geeksforgeeks.org/user/daminikaks8l",
+      linkedin: "https://www.linkedin.com/in/damini-kalal-46084128a/",
+      leetcode: "https://leetcode.com/daminikalal/",
+      github: "https://github.com/daminukalal",
+    },
+
+    imageSrc: "https://i.postimg.cc/Gt8B79Xp/Daminikalal.jpg",
+  },
+  {
+    name: "Harsh Jain",
+    job: "Frontend Developer",
+    socialMedia: {
+      geekforgeeks: "https://auth.geeksforgeeks.org/user/harshjai44gr",
+      linkedin: "https://www.linkedin.com/in/harsh-d-jain/",
+      leetcode: "https://leetcode.com/Harsh76200/",
+      github: "https://github.com/Harsh76200",
+    },
+
+    imageSrc: "https://i.postimg.cc/52ZgqHFr/profile-pic.jpg",
+  },
+];
+
+export default teamMembers;
