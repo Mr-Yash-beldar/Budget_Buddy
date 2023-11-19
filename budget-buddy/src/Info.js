aec0882cf7a989ded45e9ba3c8c888da80e25a87
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-11-19 23:49:03 +0530

    added slider info data

---
+++ budget-buddy/src/Info.js
@@ -0,0 +1,23 @@
+const sliders = [
+    {
+      title: "Budgeting: Where Dreams Meet Reality.",
+      desc:
+        "Start by tracking your income and expenses. Create a budget that outlines how much you can spend in various categories like rent, groceries, transportation, and entertainment.",
+      image: "assets/images/slider-img.png", 
+    },
+    {
+      title: "Don't Just Spend, Budget It!",
+      desc:
+        "Identify non-essential expenses and consider cuttingback on them. This could include eating out less, canceling unused subscriptions, or finding more affordable alternatives.",
+      image: "assets/images/bg.jpeg", // Replace with the actual image source
+    },
+    {
+      title: "Secure Your Future with Budgeting Today.",
+      desc:
+        " Saving for the future should stay in the forefront of your mind, and your finances, regardless of whatever else comes around. So start from today infact now onwards...",
+      image: "assets/images/w2.png", // Replace with the actual image source
+    },
+  ];
+  
+  export default sliders;
+  
