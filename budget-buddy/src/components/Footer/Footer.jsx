72d51f48063cc92317d254379739f17e22a17e47
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-11-19 23:52:55 +0530

    added footer component

---
+++ budget-buddy/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <section className="footer_section">
+          <br />
+          <div className="container">
+            <p>
+              &copy; <span id="displayYear"></span> All Rights Reserved By:- ARDHY
+              
+            </p>
+          </div>
+        </section>
+  )
+}
+
+export default Footer
